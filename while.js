a1d03da7b94b9573ab7e8ba93da0fc20d881c558
Meher09 <mehernigarcu@gmail.com>
2022-08-02 00:53:28 +0600

    while lopp practice done:

---
+++ while.js
@@ -0,0 +1,11 @@
+var roastGiven = 0;
+roastGiven = roastGiven + 1;
+roastGiven += 1;
+while (roastGiven < 7) {
+    console.log('Roast Den, please!!')
+    console.log(roastGiven);
+    roastGiven++;
+
+
+
+}
